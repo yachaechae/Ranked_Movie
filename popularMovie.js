b4dbe68876a179a5afa4e5382d8b94c6be7d2fb2
yachaechae <wpql1207@gmail.com>
2023-10-19 20:44:35 +0900

    검색 버튼 클릭 시 기존 movieCard 제거 및 검색 api 통신확인

--- popularMovie.js
+++ popularMovie.js
@@ -1,4 +1,5 @@
 const url = `https://api.themoviedb.org/3/movie`;
+const searchUrl = `https://api.themoviedb.org/3/search/movie`;
 const imgUrl = `https://image.tmdb.org/t/p/original`
 
 const options = {
@@ -9,19 +10,24 @@ const options = {
   	}
 };
 
+const searchOptions = {
+	method: 'GET',
+	headers: {
+	  	accept: 'application/json',
+	  	Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmMyZGZhZDQ3YThlZDRmMWUwYWQxYjc1MGVhMzBhMSIsInN1YiI6IjY1MzA5NGQ3YWVkZTU5MDE0YzM4MDBjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F6qLqVKcX12Lxl8WUe5P3sDfhlIdJ44DaMgj0Dvuq1M'
+	}
+  };
+
   //영화 리스트
 fetch(`${url}/popular`, options)
 	.then(response => response.json())
 	.then(response => {
 
-		let data = response.results;
-		data.map((res) => {
+		response.results.map((res) => {
 			let postImg = `${imgUrl}/${res.poster_path}`
 			let movieTitle = res.title;
 			let voteAverage = res.vote_average;
 			let overView = res.overview;
-
-			console.log(res)
 
 			const movieCard = document.createElement('div');
 			movieCard.className = 'movieCard';
@@ -33,8 +39,35 @@ fetch(`${url}/popular`, options)
 									<div class="voteAverage">${voteAverage}</div>
 									<div class="overView">${overView}</div>`
 
-			return document.querySelector(".movieList").appendChild(movieCard);
+			return document.querySelector("#movieList").appendChild(movieCard);
 		})
 
-  })
-  .catch(err => console.error(err));
+  	})
+  	.catch(err => console.error(err));
+
+
+const clearCard = () => {
+	const cardList = document.querySelectorAll(`.movieCard`)
+	cardList.forEach(element => {
+		element.remove()
+	});
+}
+
+// 영화 검색
+const searchQuery = () => {
+	let searchParams = document.querySelector('#search').value;
+	clearCard()
+
+	findMovie(searchParams)
+}
+
+
+
+const findMovie = (searchParams) => {
+	fetch(`${searchUrl}?query=${searchParams}&include_adult=false&language=en-US&page=1`, searchOptions)
+
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+
+}
